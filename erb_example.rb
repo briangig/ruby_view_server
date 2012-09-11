dcdb6c470a5532412d864e6f61ee31e52acd8086
Brian Giguere <brian@briangig.com>
2012-09-11 08:27:46 -0400

    homework #1 assisgnment

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,14 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+y = x + 23 + 90
+bingo = ERB.new "<div class='main'>
+<h3>Hi, the value you are looking for is <%= y %>
+</h3></div>"
+puts bingo.result(binding)
+
+z = 'This is '
+w = ' programming language'
+obvious = ERB.new "<b> <%= z %> the Ruby <%= w %> </b>"
+puts obvious.result(binding)
